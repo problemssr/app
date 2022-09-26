4b10d6a986279627395f0330bca7535937760365
yyt <2298269347@qq.com>
2022-09-26 20:04:43 +0800

    mock

--- src/mock/mockServe.js
+++ src/mock/mockServe.js
@@ -6,5 +6,5 @@ import banner from './banner.json'
 import floor from './floor.json'
 
 // mock数据：第一个参数请求地址，第二个参数请求数据
-Mock.mock("/mock/banner",{code:200,data:banner})//模拟轮播图数据
-Mock.mock("/mock/floor",{code:200,data:floor})
+Mock.mock("/mock/banner", { code: 200, data: banner })//模拟轮播图数据
+Mock.mock("/mock/floor", { code: 200, data: floor })
